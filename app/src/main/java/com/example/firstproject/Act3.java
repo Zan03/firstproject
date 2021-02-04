edbb0fc7d9e947d0bf5073a7b32363abfaf92d37
petik <mizanfatur1900@gmail.com>
2021-02-04 10:07:59 +0700

    menambah

--- app/src/main/java/com/example/firstproject/Act3.java
+++ app/src/main/java/com/example/firstproject/Act3.java
@@ -3,6 +3,7 @@ package com.example.firstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Act3 extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class Act3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act3);
+
+        TextView tvNama = findViewById(R.id.tv_nama);//tv_get_nama
+        TextView tvAlamat = findViewById(R.id.tv_alamat);//tv_get_alamat
+        TextView tvTTl = findViewById(R.id.tv_hoby);//tv_get_ttl
+
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras == null){
+            return;
+        }
+
+        String nama = extras.getString("nama");
+        String alamat = extras.getString("alamat");
+        String ttl = extras.getString("ttl");
+
+        tvNama.setText(nama);
+        tvAlamat.setText(alamat);
+        tvTTl.setText(ttl);
+
+        if (nama != null){
+            tvNama.setText(nama);
+        }
+
+        if (alamat != null){
+            tvAlamat.setText(alamat);
+        }
+
+        if (ttl != null){
+            tvTTl.setText(ttl);
+        }
     }
 }
